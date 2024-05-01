bccc13705ff52f3843425f791b292a87c6e780cc
Kai Dewey <deweykai5@gmail.com>
2024-04-30 21:22:46 -0600

    refactoring

--- src/main.zig
+++ src/main.zig
@@ -8,117 +8,171 @@ const MapPosition = struct { x: f32, y: f32 };
 const GridPosition = struct { x: i32, y: i32 };
 const Length = struct { width: f32, height: f32 };
 
-const SquareIterator = struct {
+const GridIterator = struct {
+    grid: *GridLayer,
     x: i32,
     y: i32,
     low_bounds: GridPosition,
     high_bounds: GridPosition,
-    layer: *Layer,
 
-    fn next(self: *SquareIterator) ?*f32 {
+    fn next(self: *GridIterator) ?*f32 {
+        if (self.y > self.high_bounds.y) {
+            return null;
+        }
+
+        const idx = self.grid.get_index(GridPosition{ .x = self.x, .y = self.y });
+        const value = &self.grid.data[idx];
+
         if (self.x > self.high_bounds.x) {
             self.x = self.low_bounds.x;
-
-            if (self.y > self.high_bounds.y) {
-                return null;
-            }
-
-            const pos = GridPosition{ .x = self.x, .y = self.y };
             self.y += 1;
-            const idx = self.layer.get_index(GridPosition, pos);
-            return &self.layer.data[idx];
+        } else {
+            self.x += 1;
         }
-        const pos = GridPosition{ .x = self.x, .y = self.y };
-        self.x += 1;
-        const idx = self.layer.get_index(GridPosition, pos);
-        return &self.layer.data[idx];
+
+        return value;
     }
 };
 
-const Layer = struct {
-    size: Length,
-    grid_width: i32,
-    grid_height: i32,
-    resolution: f32,
-    center: MapPosition,
+const GridLayer = struct {
+    width: i32,
+    height: i32,
     data: []f32,
+    offset_x: i32,
+    offset_y: i32,
 
-    fn create(alloc: Allocator, size: Length, resolution: f32) !Layer {
-        const grid_width = @as(i32, @intFromFloat(size.width / resolution)) + 1;
-        const grid_height = @as(i32, @intFromFloat(size.height / resolution)) + 1;
-        const data = try alloc.alloc(f32, @intCast(grid_width * grid_height));
+    fn create(alloc: Allocator, width: i32, height: i32) !GridLayer {
+        const data = try alloc.alloc(f32, @intCast(width * height));
         for (data) |*cell| {
             cell.* = std.math.nan(f32);
         }
-        return Layer{ .size = size, .grid_width = grid_width, .grid_height = grid_height, .resolution = resolution, .center = MapPosition{ .x = 0.0, .y = 0.0 }, .data = data };
+        return GridLayer{ .width = width, .height = height, .data = data, .offset_x = -@divFloor(width, 2), .offset_y = -@divFloor(height, 2) };
     }
 
-    fn free(grid_map: *Layer, alloc: Allocator) void {
-        alloc.free(grid_map.data);
+    fn free(self: *GridLayer, alloc: Allocator) void {
+        alloc.free(self.data);
     }
 
-    fn square_iterator(self: *Layer, center: MapPosition, size: Length) SquareIterator {
-        var low_bounds = GridPosition{ .x = @intFromFloat(center.x - size.width / 2), .y = @intFromFloat(center.y - size.height / 2) };
-        var high_bounds = GridPosition{ .x = @intFromFloat(center.x + size.width / 2), .y = @intFromFloat(center.y + size.height / 2) };
-
-        low_bounds.x = @max(low_bounds.x, @as(i32, @intFromFloat((self.center.x - self.size.width) / self.resolution)));
-        low_bounds.y = @max(low_bounds.y, @as(i32, @intFromFloat((self.center.y - self.size.height) / self.resolution)));
-
-        high_bounds.x = @min(high_bounds.x, @as(i32, @intFromFloat((self.center.x + self.size.width) / self.resolution)));
-        high_bounds.y = @min(high_bounds.y, @as(i32, @intFromFloat((self.center.y + self.size.height) / self.resolution)));
-
-        return SquareIterator{ .x = low_bounds.x, .y = low_bounds.y, .low_bounds = low_bounds, .high_bounds = high_bounds, .layer = self };
+    fn fill(self: *GridLayer, value: f32) void {
+        for (self.data) |*cell| {
+            cell.* = value;
+        }
     }
 
-    fn recenter(self: *Layer, center: MapPosition) void {
-        const low_bounds = MapPosition{ .x = self.center.x - self.size.width / 2, .y = self.center.y - self.size.height / 2 };
-        const high_bounds = MapPosition{ .x = self.center.x + self.size.width / 2, .y = self.center.y + self.size.height / 2 };
+    fn reposition(self: *GridLayer, offset_x: i32, offset_y: i32) void {
+        const low_bounds = GridPosition{ .x = self.offset_x, .y = self.offset_y };
+        const high_bounds = GridPosition{ .x = self.offset_x + self.width, .y = self.offset_y + self.height };
 
-        self.center = center;
+        self.offset_x = offset_x;
+        self.offset_y = offset_y;
 
-        var x: f32 = low_bounds.x;
+        var x: i32 = low_bounds.x;
 
-        while (x < (high_bounds.x + self.resolution / 2)) : (x += self.resolution) {
-            var y: f32 = low_bounds.y;
-            while (y < (high_bounds.y + self.resolution / 2)) : (y += self.resolution) {
-                if (self.is_valid(MapPosition{ .x = x, .y = y })) {
+        while (x <= high_bounds.x) : (x += 1) {
+            var y: i32 = low_bounds.y;
+            while (y <= high_bounds.y) : (y += 1) {
+                if (self.is_valid(GridPosition{ .x = x, .y = y })) {
                     continue;
                 }
-                const idx = self.get_index(MapPosition, MapPosition{ .x = x, .y = y });
+                const idx = self.get_index(GridPosition{ .x = x, .y = y });
                 self.data[idx] = std.math.nan(f32);
             }
         }
     }
+    fn is_valid(self: *GridLayer, pos: GridPosition) bool {
+        if ((pos.x - self.offset_x) < 0) {
+            return false;
+        }
+        if ((pos.x - self.offset_x) > self.width) {
+            return false;
+        }
+        if ((pos.y - self.offset_y) < 0) {
+            return false;
+        }
+        if ((pos.y - self.offset_y) > self.height) {
+            return false;
+        }
+        return true;
+    }
 
-    fn map_to_grid_space(self: *Layer, pos: MapPosition) GridPosition {
+    fn get_index(self: *GridLayer, pos: GridPosition) u32 {
+        const idx_x: i32 = @intCast(@mod(pos.x, self.width));
+        const idx_y: i32 = @intCast(@mod(pos.y, self.height));
+
+        const idx = idx_y * self.width + idx_x;
+        return @intCast(idx);
+    }
+
+    fn get_value(self: *GridLayer, pos: GridPosition) ?*f32 {
+        if (!self.is_valid(pos)) {
+            return null;
+        }
+
+        const idx = self.get_index(pos);
+        return &self.data[idx];
+    }
+
+    fn square_iterator(self: *GridLayer, low_bounds: GridPosition, high_bounds: GridPosition) GridIterator {
+        var checked_low_bounds = low_bounds;
+        checked_low_bounds.x = @max(checked_low_bounds.x, self.offset_x);
+        checked_low_bounds.y = @max(checked_low_bounds.y, self.offset_y);
+
+        var checked_high_bounds = high_bounds;
+        checked_high_bounds.x = @min(checked_high_bounds.x, self.offset_x + self.width);
+        checked_high_bounds.y = @min(checked_high_bounds.y, self.offset_y + self.height);
+        return GridIterator{ .grid = self, .x = low_bounds.x, .y = low_bounds.y, .low_bounds = checked_low_bounds, .high_bounds = checked_high_bounds };
+    }
+};
+
+const MapLayer = struct {
+    size: Length,
+    resolution: f32,
+    center: MapPosition,
+    grid: GridLayer,
+
+    fn create(alloc: Allocator, size: Length, resolution: f32) !MapLayer {
+        const grid_width = @as(i32, @intFromFloat(size.width / resolution)) + 1;
+        const grid_height = @as(i32, @intFromFloat(size.height / resolution)) + 1;
+        const grid = try GridLayer.create(alloc, grid_width, grid_height);
+        return MapLayer{ .size = size, .resolution = resolution, .center = MapPosition{ .x = 0.0, .y = 0.0 }, .grid = grid };
+    }
+
+    fn free(self: *MapLayer, alloc: Allocator) void {
+        self.grid.free(alloc);
+    }
+
+    fn fill(self: *MapLayer, value: f32) void {
+        self.grid.fill(value);
+    }
+
+    fn recenter(self: *MapLayer, center: MapPosition) void {
+        self.center = center;
+        const offset_x = @as(i32, @intFromFloat(center.x / self.resolution)) - @divFloor(self.grid.width, 2);
+        const offset_y = @as(i32, @intFromFloat(center.y / self.resolution)) - @divFloor(self.grid.height, 2);
+
+        self.grid.reposition(offset_x, offset_y);
+    }
+
+    fn map_to_grid_space(self: *MapLayer, pos: MapPosition) GridPosition {
         const grid_x = (pos.x + self.resolution / 2) / self.resolution;
         const grid_y = (pos.y + self.resolution / 2) / self.resolution;
 
         return GridPosition{ .x = @intFromFloat(grid_x), .y = @intFromFloat(grid_y) };
     }
 
-    fn is_valid(self: *Layer, map_pos: MapPosition) bool {
-        if ((map_pos.x - self.center.x) > ((self.size.width + self.resolution) / 2.0)) {
-            return false;
-        }
-        if ((map_pos.x - self.center.x) < -((self.size.width + self.resolution) / 2.0)) {
-            return false;
-        }
-        if ((map_pos.y - self.center.y) > ((self.size.height + self.resolution) / 2.0)) {
-            return false;
-        }
-        if ((map_pos.y - self.center.y) < -((self.size.height + self.resolution) / 2.0)) {
-            return false;
-        }
-        return true;
+    fn grid_to_map_space(self: *MapLayer, pos: GridPosition) MapPosition {
+        const map_x = @as(f32, @floatFromInt(pos.x)) * self.resolution;
+        const map_y = @as(f32, @floatFromInt(pos.y)) * self.resolution;
+        return MapPosition{ .x = map_x, .y = map_y };
     }
 
-    fn get_index(self: *Layer, comptime T: type, pos: T) u32 {
-        const grid_pos = switch (T) {
-            MapPosition => self.map_to_grid_space(pos),
-            GridPosition => pos,
-            else => unreachable,
-        };
+    fn is_valid(self: *MapLayer, map_pos: MapPosition) bool {
+        const grid_pos = self.map_to_grid_space(map_pos);
+        return self.grid.is_valid(grid_pos);
+    }
+
+    fn get_index(self: *MapLayer, pos: MapPosition) u32 {
+        const grid_pos = self.map_to_grid_space(pos);
 
         const idx_x: i32 = @intCast(@mod(grid_pos.x, self.grid_width));
         const idx_y: i32 = @intCast(@mod(grid_pos.y, self.grid_height));
@@ -127,28 +181,30 @@ const Layer = struct {
         return @intCast(idx);
     }
 
-    fn get_value(self: *Layer, pos: MapPosition) ?*f32 {
-        if (!self.is_valid(pos)) {
-            return null;
-        }
+    fn get_value(self: *MapLayer, pos: MapPosition) ?*f32 {
+        const grid_pos = self.map_to_grid_space(pos);
+        return self.grid.get_value(grid_pos);
+    }
 
-        const idx = self.get_index(MapPosition, pos);
-        return &self.data[idx];
+    fn square_iterator(self: *MapLayer, low_bounds: MapPosition, high_bounds: MapPosition) GridIterator {
+        const low_grid_pos = self.map_to_grid_space(low_bounds);
+        const high_grid_pos = self.map_to_grid_space(high_bounds);
+        return self.grid.square_iterator(low_grid_pos, high_grid_pos);
     }
 };
 
 test "create grid map" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
-    try expectEqual(@as(i32, 101), a.grid_height);
-    try expectEqual(@as(i32, 201), a.grid_width);
+    try expectEqual(@as(i32, 101), a.grid.height);
+    try expectEqual(@as(i32, 201), a.grid.width);
 }
 
 test "grid bounds check" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     var x: f32 = -10;
@@ -164,7 +220,7 @@ test "grid bounds check" {
 
 test "grid non zero center bounds check" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     a.recenter(MapPosition{ .x = 5.0, .y = 0.0 });
@@ -182,7 +238,7 @@ test "grid non zero center bounds check" {
 
 test "get grid value" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     var x: f32 = -10;
@@ -203,7 +259,7 @@ test "get grid value" {
 
 test "read uninitialized grid value" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     try testing.expect(std.math.isNan(a.get_value(MapPosition{ .x = 0.0, .y = 0.0 }).?.*));
@@ -211,7 +267,7 @@ test "read uninitialized grid value" {
 
 test "write read grid value" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     {
@@ -224,7 +280,7 @@ test "write read grid value" {
 }
 test "write move and read grid value" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     a.get_value(MapPosition{ .x = 0.0, .y = 0.0 }).?.* = 1.0;
@@ -234,7 +290,7 @@ test "write move and read grid value" {
 
 test "check moving out of bounds clears data" {
     const alloc = testing.allocator;
-    var a = try Layer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(alloc, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(alloc);
 
     a.get_value(MapPosition{ .x = 0.0, .y = 0.0 }).?.* = 1.0;
@@ -244,11 +300,32 @@ test "check moving out of bounds clears data" {
 }
 
 test "square_iterator" {
-    var a = try Layer.create(testing.allocator, Length{ .width = 10, .height = 5 }, 0.05);
+    var a = try MapLayer.create(testing.allocator, Length{ .width = 10, .height = 5 }, 0.05);
     defer a.free(testing.allocator);
 
-    var iter = a.square_iterator(MapPosition{ .x = 0.0, .y = 0.0 }, Length{ .width = 1.0, .height = 1.0 });
+    var iter = a.square_iterator(MapPosition{ .x = 0.0, .y = -1.0 }, MapPosition{ .x = 1.0, .y = 2.0 });
     while (iter.next()) |cell| {
         cell.* = 1.0;
+    }
+
+    var x: f32 = -10;
+    while (x < 10) : (x += 0.1) {
+        var y: f32 = -10;
+        while (y < 10) : (y += 0.1) {
+            const expect_value = (-0.025 <= x) and (x <= 1.025) and (-1.025 <= y) and (y <= 2.025);
+            if (a.get_value(MapPosition{ .x = x, .y = y })) |cell| {
+                if (expect_value) {
+                    testing.expect(cell.* == 1.0) catch |err| {
+                        std.debug.print("x: {}, y: {}, expected: 1.0\n", .{ x, y });
+                        return err;
+                    };
+                } else {
+                    testing.expect(std.math.isNan(cell.*)) catch |err| {
+                        std.debug.print("x: {}, y: {}, expected: nan\n", .{ x, y });
+                        return err;
+                    };
+                }
+            }
+        }
     }
 }
