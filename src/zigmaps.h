9eaed29a96a70b1e36fd8019421cc9a63571ab83
Kai Dewey <deweykai5@gmail.com>
2024-05-03 03:06:12 -0600

    cleanup header file

--- src/zigmaps.h
+++ src/zigmaps.h
@@ -1,16 +1,21 @@
 #ifndef ZIGMAPS_H
 #define ZIGMAPS_H
 
-struct MapLayer;
+#if __cplusplus
+extern "C" {
+#endif
 
-extern struct MapLayer *zigmaps_create(float const width, float const height,
-                                       float const center_x,
-                                       float const center_y,
-                                       float const resolution);
-extern void zigmaps_free(struct MapLayer *const map);
-extern float *zigmaps_at(struct MapLayer *const map, float const x,
-                         float const y);
-extern struct MapLayer const *
-zigmaps_make_traverse(struct MapLayer const *const map);
+struct MapLayer;
+typedef struct MapLayer MapLayer;
+
+extern MapLayer *zigmaps_create(float width, float height, float center_x,
+                                float center_y, float resolution);
+extern void zigmaps_free(MapLayer *map);
+extern float *zigmaps_at(MapLayer *map, float x, float y);
+extern MapLayer *zigmaps_make_traverse(MapLayer *map);
+
+#if __cplusplus
+}
+#endif
 
 #endif
